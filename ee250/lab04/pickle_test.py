378f0dfcf345e77f43daf67e2cd9c19380f061aa
Nelson Gonzalez <nelsongo@usc.edu>
2020-09-14 19:15:40 -0700

    Update pickle_test.py

--- ee250/lab04/pickle_test.py
+++ ee250/lab04/pickle_test.py
@@ -6,9 +6,17 @@ notes = []
 
 # A. If notes.pickle exists, read it in using pickle and assign the content to
 #   the notes variable
+if notes.pickle:
+    for n in notes.pickle:
+        notes[n] = notes.pickle[n]
 
 # B. Print out notes
-
+    for x in range(len(notes)):
+        print(notes[x])
 # C. Read in a string from the user using input() and append it to notes
-
+    user_input = input("Enter string ")
+    notes.append(user_input)
 # D. Save notes to notes.pickle
+    notes.pickle = [None] * len(notes)
+    for i in notes:
+        notes.pickle[i] = notes[i]
