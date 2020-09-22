fd52527df6ddd5a402e27ce4387e676e661e5d91
Nelson Gonzalez <nelsongo@usc.edu>
2020-09-21 21:47:08 -0700

    Update rpi_pub_and_sub.py

--- ee250/lab05/rpi_pub_and_sub.py
+++ ee250/lab05/rpi_pub_and_sub.py
@@ -4,18 +4,24 @@ Run rpi_pub_and_sub.py on your Raspberry Pi."""
 import paho.mqtt.client as mqtt
 import time
 import grovepi
-button = 3
+button = 2
+led = 3
+ulltrasonic_ranger = 4
 def on_connect(client, userdata, flags, rc):
     print("Connected to server (i.e., broker) with result code "+str(rc))
     #subscribe to topics of interest here
     client.subscribe("raspberrypy/custom_callback")
     client.message_callback_add("raspberrypi/custom_callback",custom_callback)
     client.subscribe("raspberrypi/ultrasonicRanger")
+    client.subcribe("raspberrypi/led")
 #Default message callback. Please use custom callbacks.
 def custom_callback(client, userdata, message):
     print("Custom_callback: " + message.topic + " " + "\"" + str(message.payload, "utf-8") + "\"")
     print("custom_callback: message.payload is of type " + str(type(message.payload)))
-
+    if str(message.payload) == "LED_ON":
+        digitalWrite(led,1)
+    if str(message.payload) == "LED_OFF":
+        digitalWrite(led,0)
 def on_message(client, userdata, msg):
     print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
 
@@ -31,8 +37,10 @@ if __name__ == '__main__':
     while True:
         #print("delete this line")
         time.sleep(1)
+        client.publish("raspberrypi/ultrasonicRanger",grovepi.ultrasonicRead(ultrasonic_ranger))
         if grovepi.pinMode(button,"INPUT") == 0:
             client.publish("raspberrypi/button","Button pressed!")
+
 
 
 
