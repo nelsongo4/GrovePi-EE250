8406bdc1262bb89b0c155f152492981cc8346fad
Nelson Gonzalez <nelsongo@usc.edu>
2020-09-29 08:59:20 -0700

    Update my_reddit.py

--- ee250/lab06/my_reddit.py
+++ ee250/lab06/my_reddit.py
@@ -16,14 +16,14 @@ def qotd_init():
     response = requests.get('http://www.reddit.com/r/quotes/random.json',
                             params=params, headers=headers)
 
-   if response.status_code == 200: # Status: OK
+    if response.status_code == 200: # Status: OK
         data = response.json()
         # TODO: Extract the quote from the data. Use pretty printing to help you decipher the json
         quote = 'This is supposed to be quote'
         print(type(data))
         #print(data)
         hello = data[0]
-        print(type(hello))
+        #print(type(hello))
         #key_list = list(hello)
         #values = hello.values()
         #values_list = list(values)
@@ -33,25 +33,26 @@ def qotd_init():
         #print(key_list[1])
         res = {key: hello[key] for key in hello.keys() 
                                & {'data'}}
-        print(type(res))
-        print(json.dumps(res, sort_keys = True, indent = 4))
+        #print(type(res))
+        #print(json.dumps(res, sort_keys = True, indent = 4))
         search = res["data"]
-        print(type(search))
+        #print(type(search))
         #print(json.dumps(search, sort_keys = True, indent = 4))
         new_search = search["children"]
         for super_search in new_search:
-            print(super_search)
-        print(json.dumps(super_search, sort_keys = True, indent = 4))
-        print(type(new_search))
+            pass 
+            #print(super_search)
+        #print(json.dumps(super_search, sort_keys = True, indent = 4))
+        #print(type(new_search))
         deeper_search = super_search["data"]
-        print(" ")
-        print(" ")
-        print(json.dumps(deeper_search, sort_keys = True, indent = 4))
+        #print(" ")
+        #print(" ")
+        #print(json.dumps(deeper_search, sort_keys = True, indent = 4))
         #print(new_search)
         last_search = deeper_search["url"]
         quote_search = deeper_search["title"]
-        print(quote_search)
-        print(last_search)
+        #print(quote_search)
+        #print(last_search)
         quote = quote_search
         #final_search = requests.get(last_search, params = params,headers = headers)
         #final_search = requests.get(last_search)
@@ -73,6 +74,8 @@ QOTD_APP = {
 
 if __name__ == '__main__':
     qotd_init()
+
+
 
 
 
